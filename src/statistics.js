3701d070a073e2c2c5dff7356965f124e8c912df
Sonali Mahendrakar <114967822+SonaliM2000@users.noreply.github.com>
2024-05-01 17:25:31 -0400

    statistics.js

--- src/statistics.js
+++ src/statistics.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './statistics.css'
+import './statistics.css';
 import { Bar } from 'react-chartjs-2';
-import {Chart,CategoryScale, LinearScale, BarElement} from "chart.js";
-Chart.register(CategoryScale, LinearScale, BarElement)
+import { Chart, CategoryScale, LinearScale, BarElement } from 'chart.js';
+Chart.register(CategoryScale, LinearScale, BarElement);
+
 export function Statistics() {
   const [statistics, setStatistics] = useState(null);
 
@@ -21,11 +22,13 @@ export function Statistics() {
   };
 
   return (
-      
-    
-      
-      <div classname="stats">
+    <div className="stats">
+      <br></br><br></br>
       <h1>Statistics</h1>
+      <nav>
+          <span className="Logo">Wanderly !</span>
+          
+        </nav>
       {statistics && (
         <div>
           {/* Render statistics data here */}
@@ -33,30 +36,34 @@ export function Statistics() {
             <h2>Average Rating</h2>
             <p>{statistics.average_rating}</p>
           </div>
-          <div>
-            <h2>User Vlog Counts</h2>
-            <Bar
-              data={{
-                labels: Object.keys(statistics.user_vlog_counts),
-                datasets: [
-                  {
-                    label: 'Number of Vlogs',
-                    data: Object.values(statistics.user_vlog_counts),
-                    backgroundColor: 'rgba(75,192,192,1)',
-                    borderWidth: 1,
-                  },
-                ],
-              }}
-              options={{
-                scales: {
-                  yAxes: [{
-                    ticks: {
-                      beginAtZero: true,
+          <div className="graph-container">
+            <div className="graph">
+              <h2>User Vlog Counts</h2>
+              <Bar
+                data={{
+                  labels: Object.keys(statistics.user_vlog_counts),
+                  datasets: [
+                    {
+                      label: 'Number of Vlogs',
+                      data: Object.values(statistics.user_vlog_counts),
+                      backgroundColor: 'rgb(10, 11, 12)',
+                      borderWidth: 1,
                     },
-                  }],
-                },
-              }}
-            />
+                  ],
+                }}
+                options={{
+                  scales: {
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                      },
+                    ],
+                  },
+                }}
+              />
+            </div>
           </div>
         </div>
       )}
